928d691a5666de8b7fc7979a34d93bce87324610
benjaminpasic <benjamin.pasic@stu.ibu.edu.ba>
2021-03-19 00:10:02 +0100

    added bll

--- api/index.php
+++ api/index.php
@@ -5,10 +5,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once dirname(__FILE__).'/../vendor/autoload.php';
-require_once dirname(__FILE__).'/dao/AccountDao.class.php';
+require_once dirname(__FILE__).'/services/AccountService.class.php';
 require_once dirname(__FILE__).'/routes/accounts.php';
 
 Flight::register('accountDao','AccountDao');
+Flight::register('accountService','AccountService');
 
 Flight::map('query',function($name, $default_value = NULL){
     $request = Flight::request();
--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -6,15 +6,8 @@ Flight::register('accountDao','AccountDao');
 Flight::route('GET /accounts', function(){
   $offset = Flight::query('offset',0);
   $limit = Flight::query('limit',10);
-
   $search = Flight::query('search');
-
-  if($search){
-    Flight::json(Flight::accountDao()->get_accounts($search,$offset,$limit));
-  }else{
-    Flight::json(Flight::accountDao()->get_all($offset,$limit));
-  }
-
+  Flight::json(Flight::accountService()->get_accounts($search,$offset,$limit));
 });
 
 //Get single account by id
